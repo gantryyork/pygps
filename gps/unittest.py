433c162c64f6256024a106849bad4ba133b2925b
Gantry York <gantry.engineer@gmail.com>
2021-10-25 16:45:20 -0700

    beginning unit tests

---
+++ gps/unittest.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+class TestConvert(TestCase):
+
+    def test_nm_to_km_positive(self):
+        self.assertTrue(True)
+
+    def test_nm_to_km_negative(self):
+        self.assertTrue(True)
+
+    def test_km_to_nm_positive(self):
+        self.assertTrue(True)
+
+    def test_km_to_nm_negative(self):
+        self.assertTrue(True)
