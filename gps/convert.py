adc2587843c1bf68c17b0d5447c77f052de82661
Gantry York <gantry.engineer@gmail.com>
2021-10-25 16:31:00 -0700

    Initial commit

---
+++ gps/convert.py
@@ -0,0 +1,29 @@
+def dec_to_dms(decimal):
+    """
+    Takes any decimal number and converts it to degrees, minutes, seconds,
+    direction.
+
+    Args:
+        decimal (float): a number representing half of a GPS Coordinate
+    """
+    pass
+
+
+def dms_to_dec(degrees, minutes, seconds, direction):
+    pass
+
+
+def dec_to_longitude(decimal):
+    pass
+
+
+def dec_to_latitude(decimal):
+    pass
+
+
+def nm_to_km(nm):
+    pass
+
+
+def km_to_nm(km):
+    pass
---
+++ gps/coordinate.py
@@ -0,0 +1,15 @@
+class Coordinate(object)
+
+
+def __init__(self, lat=0.0, lon=0.0):
+
+    self.latitude = lat
+    self.longitude = lon
+
+
+def distance(self, dest_lat, dest_lon):
+    pass
+
+
+def add(self, heading, distance):
+    pass
