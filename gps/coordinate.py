bb70b958c39ceff82a838bc7724f840528d7e28f
Gantry York <gantry.engineer@gmail.com>
2021-10-25 19:26:45 -0700

    Addint many functions

--- gps/coordinate.py
+++ gps/coordinate.py
@@ -1,15 +1,12 @@
-class Coordinate(object)
+class Coordinate(object):
 
+    def __init__(self, lat=0.0, lon=0.0):
 
-def __init__(self, lat=0.0, lon=0.0):
+        self.latitude = lat
+        self.longitude = lon
 
-    self.latitude = lat
-    self.longitude = lon
+    def distance(self, dest_lat, dest_lon):
+        pass
 
-
-def distance(self, dest_lat, dest_lon):
-    pass
-
-
-def add(self, heading, distance):
-    pass
+    def add(self, heading, distance):
+        pass
